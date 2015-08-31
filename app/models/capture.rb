9ea59e7e57f924fe30e939089202d0b0a0fadeaf
Omar Amiri <omar@openvelvet.com>
2015-08-31 07:46:54 -0700

    updated green button font

--- app/models/capture.rb
+++ app/models/capture.rb
@@ -1,3 +1,5 @@
 class Capture < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
+
+	validates :email, uniqueness: true, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
